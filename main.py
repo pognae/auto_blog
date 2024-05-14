38dd2da5d570cae1d1daa765d47d23dec9a5d5c6
dyl <37256129+pognae@users.noreply.github.com>
2024-05-14 15:01:09 +0900

    update

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import os
 from apscheduler.schedulers.background import BlockingScheduler
 from database import *
 # from hot_deal import getDeal
-from n2t import n2t_exe
+# from n2t import n2t_exe
+from login import login
+
 
 # load .env
 load_dotenv()
@@ -13,17 +15,19 @@ hotdeal_schedule_interval = os.environ.get('hotdeal_schedule_interval')
 
 
 def main():
+    login()
+
+'''
     # 디비 연결
     init_db()
 
-'''
     # getDeal('init') #티스토리 api 종료로 에러 발생
 
-    # hotdeal_interval = int(hotdeal_schedule_interval) * 1
+    # hotdeal_interval = int(hotdeal_schedule_interval) * 1 #티스토리 api 종료로 에러 발생
     write_interval = int(hotdeal_schedule_interval) * 24
 
     sched = BlockingScheduler(timezone='Asia/Seoul')
-    # sched.add_job(getDeal, 'interval', seconds=hotdeal_interval, id='getDeal', args=['start'])
+    # sched.add_job(getDeal, 'interval', seconds=hotdeal_interval, id='getDeal', args=['start']) #티스토리 api 종료로 에러 발생
     sched.add_job(n2t_exe, 'interval', seconds=write_interval, id='n2t_exe', args=['start'])
     sched.start()
 '''
